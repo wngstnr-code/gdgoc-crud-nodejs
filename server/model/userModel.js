3071804478c7e770aa1974ece1402ef3a0ff46b8
Wangsit Nursyahada <wangsitsada1234@gmail.com>
2025-11-25 15:46:20 +0700

    feat: add email validation to ensure it ends with @gmail.com

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -2,7 +2,17 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
+    email: {
+        type: String, 
+        required: true, 
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return v.endsWith('@gmail.com');
+            },
+            message: 'Email harus menggunakan @gmail.com'
+        }
+    },
     age: {type: Number, required: true},
     bio: {type: String}
 })
