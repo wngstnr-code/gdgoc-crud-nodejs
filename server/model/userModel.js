f7f8294e93854a81bbbbf6839e0b8225d19b0227
Wangsit Nursyahada <wangsitsada1234@gmail.com>
2025-11-25 15:50:34 +0700

    refactor: remove email validation for @gmail.com from user model

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -2,17 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
-    email: {
-        type: String, 
-        required: true, 
-        unique: true,
-        validate: {
-            validator: function(v) {
-                return v.endsWith('@gmail.com');
-            },
-            message: 'Email harus menggunakan @gmail.com'
-        }
-    },
+    email: {type: String, required: true, unique: true},
     age: {type: Number, required: true},
     bio: {type: String}
 })
